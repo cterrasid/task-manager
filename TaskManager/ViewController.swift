55148657d82d7bf08a52a016e00a5bb20d389a9d
Clarette Terrasi Díaz <clarette.terrasi@movetia.com>
2020-01-21 17:00:20 +0100

    Implement Core-Data

--- TaskManager/ViewController.swift
+++ TaskManager/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-var tasks = [String]()
+var tasks = [NSManagedObject]()
 
 class ViewController: UIViewController {
     // MARK: Properties
@@ -20,7 +21,7 @@ class ViewController: UIViewController {
         let saveTask = UIAlertAction (title: "Save", style: .default, handler: { (action: UIAlertAction) -> Void in
             let textField = alert.textFields!.first
             textField?.placeholder = "Enter new task"
-            tasks.append(textField!.text!)
+            self.saveTask(nameTask: textField!.text!)
 
             self.tableView.reloadData()
         })
@@ -37,12 +38,57 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func saveTask(nameTask: String) {
+        //1 Recuperar el Managed Object Context a partir del appDelegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //2 Crear un objeto de tipo NSEntityDescription que representa a Task y servira de referencia para el Managed Object Context
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: managedContext)
+        let task = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        //3
+        task.setValue(nameTask, forKey: "name")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            tasks.append(task)
+        } catch let error as NSError {
+            print("Can't save the task \(error), \(error.userInfo)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+}
+
+extension ViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 1
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let fetchRequest : NSFetchRequest<Task> = Task.fetchRequest()
+        
+        // 3
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            tasks = results as [NSManagedObject]
+        } catch let error as NSError {
+            print("Can't be loaded \(error), \(error.userInfo)")
+        }
+        // 4
+        tableView.reloadData()
+    }
 }
 
 extension ViewController: UITableViewDataSource {
@@ -53,7 +99,8 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell")
-        cell!.textLabel!.text = tasks[indexPath.row]
+        let task = tasks[indexPath.row]
+        cell!.textLabel!.text = task.value(forKey: "name") as? String
 
         return cell!
     }
